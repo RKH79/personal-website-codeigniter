fff1eb690083c1e3dcdac71cac6d30c2d815a459
RKH79 <rohollah1703@gmail.com>
2020-09-22 17:50:38 +0330

    edit

--- application/views/Records.php
+++ application/views/Records.php
@@ -2,7 +2,7 @@
   <div class="row text-center mx-auto" style="width: 95%;">
     <?php foreach ($records as $record): ?>
     <div class="col-xl-3 col-lg-4 col-md-6 col-sm-6">
-      <a class="downloadBox card shadow p-4 mb-4 bg-white" href="<?= $record->link ?>" download>
+      <a class="downloadBox card p-4 mb-4 bg-white" href="<?= $record->link ?>" download>
         <?= $record->title ?>
       </a>
     </div>
